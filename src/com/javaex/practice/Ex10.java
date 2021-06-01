a9f6d184db60187019d26ed3e54bb03d585ec544
Danny <ctct25@naver.com>
2021-06-01 21:49:52 +0900

    21/5/25 과제제출

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -5,8 +5,7 @@ public class Ex10 {
 	public static void main(String[] args) {
 		
 //		int x;
-//		System.out.println(x);
-
+//		System.out.println(x);   
 		
 		//오류나는 이유는 자료형 x라는 공간이 만들어졌는데 그 안에 어떠한 값도 정해지지않은 상태에서 출력하게끔 되있어서 그렇다.
 		//따라서 int x = 정수; 를 넣어주면 해결될것이다.
